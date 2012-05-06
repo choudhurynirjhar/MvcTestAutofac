67b5dae63f580a103fda59f5619983bb096c89c8
choudhurynirjhar <choudhurynirjhar@gmail.com>
2012-05-06 01:28:57 -0400

    version 1

---
+++ MvcAutofacTest/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcAutofacTest.Service;
+
+namespace MvcAutofacTest.Controllers
+{
+    public class HomeController : Controller
+    {
+        private IWeatherDataProvider weather;
+
+        public HomeController(IWeatherDataProvider weather)
+        {
+            this.weather = weather;
+        }
+
+        public ActionResult Index()
+        {
+            ViewBag.Message = weather.GetWeatherInfo(07801)["Low Temp"];
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            return View();
+        }
+    }
+}
---
+++ MvcAutofacTest/Service/MockWeatherDataProvider.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MvcAutofacTest.Service
+{
+    public class MockWeatherDataProvider : IWeatherDataProvider
+    {
+        public IDictionary<string, string> GetWeatherInfo(int zipCode)
+        {
+            IDictionary<string, string> weather = new Dictionary<string, string>();
+            weather.Add("High Temp", "90");
+            weather.Add("Low Temp", "80");
+            weather.Add("Record High", "100");
+            weather.Add("Record Low", "40");
+            return weather;
+        }
+    }
+}
